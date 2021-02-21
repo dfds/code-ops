1cb6188674c5b78a5a35ff8e3c15676938d5925e
Tobias Andersen <toban@dfds.com>
2021-02-21 16:58:39 +0100

    - Fixed TODO in IntegrationEvent

--- src/CloudEngineering.CodeOps.Abstractions/Events/IntegrationEvent.cs
+++ src/CloudEngineering.CodeOps.Abstractions/Events/IntegrationEvent.cs
@@ -4,25 +4,35 @@ using System.Text.Json.Serialization;
 
 namespace CloudEngineering.CodeOps.Abstractions.Events
 {
-	//TODO: Factor this to use JsonConstructor + init and not set accessors once we have migrated to .net 5.0
 	public class IntegrationEvent : IIntegrationEvent
 	{
 		[JsonPropertyName("id")]
-		public Guid Id { get; set; } = Guid.NewGuid();
+		public Guid Id { get; init; }
 
 		[JsonPropertyName("creationDate")]
-		public DateTime CreationDate { get; set; } = DateTime.UtcNow;
+		public DateTime CreationDate { get; init; }
 
 		[JsonPropertyName("correlationId")]
-		public Guid CorrelationId { get; set; } = Guid.NewGuid();
+		public Guid CorrelationId { get; init; }
 
 		[JsonPropertyName("schemaVersion")]
-		public int SchemaVersion { get; set; } = 1;
+		public int SchemaVersion { get; init; }
 
 		[JsonPropertyName("type")]
-		public string Type { get; set; }
+		public string Type { get; init; }
 
 		[JsonPropertyName("payload")]
-		public JsonElement? Payload { get; set; }
+		public JsonElement? Payload { get; init; }
+
+		[JsonConstructor]
+		public IntegrationEvent(Guid id, string type, JsonElement payload = default, DateTime creationDate = default, Guid correlationId = default, int schemaVersion = 1)
+		{
+			Id = id;
+			Type = type;
+			Payload = payload;
+			CreationDate = (creationDate != DateTime.MinValue) ? creationDate : DateTime.UtcNow;
+			CorrelationId = correlationId;
+			SchemaVersion = schemaVersion;
+		}
 	}
 }
