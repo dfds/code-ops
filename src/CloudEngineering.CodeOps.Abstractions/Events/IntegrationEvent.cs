cae415e7231f3ec968c7fef98d39f44096796e3b
Tobias Andersen <toban@dfds.com>
2021-03-01 13:05:06 +0100

    - Added constructor to create events with no id (will auto-generate a guid under the hood)

--- src/CloudEngineering.CodeOps.Abstractions/Events/IntegrationEvent.cs
+++ src/CloudEngineering.CodeOps.Abstractions/Events/IntegrationEvent.cs
@@ -25,6 +25,11 @@ namespace CloudEngineering.CodeOps.Abstractions.Events
 		public JsonElement? Payload { get; init; }
 
 		[JsonConstructor]
+		public IntegrationEvent(string type, JsonElement payload = default, DateTime creationDate = default, Guid correlationId = default, int schemaVersion = 1) : this(Guid.NewGuid(), type, payload, creationDate, correlationId, schemaVersion)
+		{
+		}
+
+		[JsonConstructor]
 		public IntegrationEvent(Guid id, string type, JsonElement payload = default, DateTime creationDate = default, Guid correlationId = default, int schemaVersion = 1)
 		{
 			Id = id;
