23bc4cec29d017711ef420a508d09ff4ada66564
Tobias Andersen <toban@dfds.com>
2021-03-02 21:10:02 +0100

    - Dependency module for azuredevops package.

---
+++ src/CloudEngineering.CodeOps.Infrastructure.AzureDevOps/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+
+namespace CloudEngineering.CodeOps.Infrastructure.AzureDevOps
+{
+    public static class DependencyInjection
+    {
+        public static void AddAzureDevOps(this IServiceCollection services, IConfiguration configuration)
+        {
+            //External dependencies
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            //Package dependencies
+            services.Configure<AdoClientOptions>(configuration.GetSection(AdoClientOptions.AdoClient));
+
+            services.AddTransient<IAdoClient, AdoClient>();
+        }
+    }
+}
