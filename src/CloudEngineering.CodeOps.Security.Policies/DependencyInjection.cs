5871207f4e46d0b463d1e2036b2f76904a86858f
Tobias Andersen <toban@dfds.com>
2021-03-02 20:41:40 +0100

    Dependency "module" for SecurityPolicies

---
+++ src/CloudEngineering.CodeOps.Security.Policies/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using CloudEngineering.CodeOps.Security.Policies.Handlers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CloudEngineering.CodeOps.Security.Policies
+{
+    public static class DependencyInjection
+    {
+        public static void AddSecurityPolicies(this IServiceCollection services)
+        {
+            services.AddSingleton<IAuthorizationHandler, AccessRequirementHandler>();
+            services.AddSingleton<IAuthorizationPolicyProvider, DefaultAuthorizationPolicyProvider>();
+        }
+    }
+}
