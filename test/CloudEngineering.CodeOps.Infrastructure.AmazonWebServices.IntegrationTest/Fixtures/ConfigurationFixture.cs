d63ef0551231319b1d828464d2c7e7d4a508f796
Tobias Andersen <toban@dfds.com>
2021-03-09 22:54:12 +0100

    .

--- test/CloudEngineering.CodeOps.Infrastructure.AmazonWebServices.IntegrationTest/Fixtures/ConfigurationFixture.cs
+++ test/CloudEngineering.CodeOps.Infrastructure.AmazonWebServices.IntegrationTest/Fixtures/ConfigurationFixture.cs
@@ -17,8 +17,9 @@ namespace CloudEngineering.CodeOps.Infrastructure.AmazonWebServices.IntegrationT
             Console.WriteLine($"ConfigFixture base dir: {AppDomain.CurrentDomain.BaseDirectory}");
             Console.WriteLine($"appsettings.json is in root of base dir: {File.Exists(AppDomain.CurrentDomain.BaseDirectory + "appsettings.json")}");
             Console.WriteLine($"Content of appsettings.jsonin root of base dir: {File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "appsettings.json")}");
-            Console.WriteLine($"Env var: {Environment.GetEnvironmentVariable("AwsFacade__SecretKey")}");
-            Console.WriteLine($"Env var2: {Environment.GetEnvironmentVariable("$AWSFACADE__SECRETKEY")}");
+            Console.WriteLine($"Env var: {Environment.GetEnvironmentVariable("AwsFacade_SecretKey")}");
+            Console.WriteLine($"Env var2: {Environment.GetEnvironmentVariable("AwsFacade__SecretKey")}");
+            Console.WriteLine($"Env var3: {Environment.GetEnvironmentVariable("$AWSFACADE__SECRETKEY")}");
 
             var builder = new ConfigurationBuilder()
             .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
