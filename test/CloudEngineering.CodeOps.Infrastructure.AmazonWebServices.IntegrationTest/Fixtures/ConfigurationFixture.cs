aa7b7ac544c3b04fe9c8afda7282f1daf050a533
Tobias Andersen <toban@dfds.com>
2021-03-09 22:18:16 +0100

    .

--- test/CloudEngineering.CodeOps.Infrastructure.AmazonWebServices.IntegrationTest/Fixtures/ConfigurationFixture.cs
+++ test/CloudEngineering.CodeOps.Infrastructure.AmazonWebServices.IntegrationTest/Fixtures/ConfigurationFixture.cs
@@ -12,7 +12,8 @@ namespace CloudEngineering.CodeOps.Infrastructure.AmazonWebServices.IntegrationT
         public ConfigurationFixture()
         {
             Console.WriteLine($"ConfigFixture base dir: {AppDomain.CurrentDomain.BaseDirectory}");
-            Console.WriteLine($"AppSettings.json is in root of base dir: {File.Exists(AppDomain.CurrentDomain.BaseDirectory + "appsettings.json")}");
+            Console.WriteLine($"appsettings.json is in root of base dir: {File.Exists(AppDomain.CurrentDomain.BaseDirectory + "appsettings.json")}");
+            Console.WriteLine($"Content of appsettings.jsonin root of base dir: {File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "appsettings.json")}");
 
             var builder = new ConfigurationBuilder()
             .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
