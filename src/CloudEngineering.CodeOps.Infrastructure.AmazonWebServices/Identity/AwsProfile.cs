8c3c0fcfd393ade33559f7743544b33c7a8b0fb0
Tobias Andersen <toban@dfds.com>
2021-03-09 16:56:43 +0100

    .

--- src/CloudEngineering.CodeOps.Infrastructure.AmazonWebServices/Identity/AwsProfile.cs
+++ src/CloudEngineering.CodeOps.Infrastructure.AmazonWebServices/Identity/AwsProfile.cs
@@ -2,10 +2,10 @@
 {
     public sealed class AwsProfile : IAwsProfile
     {
-        public string SourceProfile { get; init; }
+        public string SourceProfile { get; set; }
 
-        public string Name { get; init; }
+        public string Name { get; set; }
 
-        public string RoleArn { get; init; }
+        public string RoleArn { get; set; }
     }
 }
