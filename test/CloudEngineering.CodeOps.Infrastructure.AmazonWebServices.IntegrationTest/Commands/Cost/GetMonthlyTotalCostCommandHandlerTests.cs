f9ad0aec53f49959fbe927b6dd12ab17661720ab
Tobias Andersen <toban@dfds.com>
2021-03-10 20:46:41 +0100

    - Extra test to verify that fetching cost data from the Cost API is scoped by the profile used by the AWS SDK. This results in calls for all accounts to return only the cost for the given capablity AWS account and explicit calls for cost on other accounts (by id) will be obfuscated by returning 0 as the BlendedCost metric value

--- test/CloudEngineering.CodeOps.Infrastructure.AmazonWebServices.IntegrationTest/Commands/Cost/GetMonthlyTotalCostCommandHandlerTests.cs
+++ test/CloudEngineering.CodeOps.Infrastructure.AmazonWebServices.IntegrationTest/Commands/Cost/GetMonthlyTotalCostCommandHandlerTests.cs
@@ -31,13 +31,30 @@ namespace CloudEngineering.CodeOps.Infrastructure.AmazonWebServices.IntegrationT
         }
 
         [Fact]
-        public async Task CanGetMonthlyTotalCostForAccount()
+        public async Task CanGetMonthlyTotalCostForOwnAccount()
         {
             //Arrange
             var facade = _fixture.Facade;
             var command = new GetMonthlyTotalCostCommand() 
             {
                 AccountIdentifier = "642375522597"
+            };
+
+            //Act
+            var result = await facade.Execute(command);
+
+            //Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact(Skip = "It seems like the account we use for the SDK affects the result from the API. We need to use prime and the assume roles for individual accounts.")]
+        public async Task CanGetMonthlyTotalCostForOtherCapabilityAccount()
+        {
+            //Arrange
+            var facade = _fixture.Facade;
+            var command = new GetMonthlyTotalCostCommand()
+            {
+                AccountIdentifier = "571914469803"
             };
 
             //Act
