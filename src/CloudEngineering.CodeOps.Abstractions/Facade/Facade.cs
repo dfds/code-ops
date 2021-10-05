693c1f933ab0d78524c8f1a7dfdca21bed6ed853
Tobias Andersen <toban@dfds.com>
2021-10-05 11:29:34 +0200

    - Making Execute method abstract to push resolution of IMediator to implementation classes

--- src/CloudEngineering.CodeOps.Abstractions/Facade/Facade.cs
+++ src/CloudEngineering.CodeOps.Abstractions/Facade/Facade.cs
@@ -9,19 +9,6 @@ namespace CloudEngineering.CodeOps.Abstractions.Facade
 {
     public abstract class Facade : IFacade
     {
-        protected readonly IServiceScopeFactory _scopeFactory;
-
-        protected Facade(IServiceScopeFactory scopeFactory)
-        {
-            _scopeFactory = scopeFactory ?? throw new ArgumentNullException(nameof(scopeFactory));
-        }
-
-        public virtual async Task<T> Execute<T>(ICommand<T> command, CancellationToken cancellationToken = default)
-        {
-            using var scope = _scopeFactory.CreateScope();
-            var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
-
-            return await mediator.Send(command, cancellationToken);
-        }
+        public abstract Task<T> Execute<T>(ICommand<T> command, CancellationToken cancellationToken = default);
     }
 }
