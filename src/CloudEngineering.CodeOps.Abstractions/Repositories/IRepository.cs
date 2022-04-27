2e2125377949b41fa73d7bb9c08bb8a7c7e48bea
Tobias Andersen <toban@dfds.com>
2022-04-27 22:38:26 +0200

    - Added cancellationtoken to GetAsync in IRepository

--- src/CloudEngineering.CodeOps.Abstractions/Repositories/IRepository.cs
+++ src/CloudEngineering.CodeOps.Abstractions/Repositories/IRepository.cs
@@ -3,13 +3,14 @@ using CloudEngineering.CodeOps.Abstractions.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CloudEngineering.CodeOps.Abstractions.Repositories
 {
     public interface IRepository<TAggregate> : IRepository where TAggregate : IAggregateRoot
     {
-        Task<IEnumerable<TAggregate>> GetAsync(Expression<Func<TAggregate, bool>> filter);
+        Task<IEnumerable<TAggregate>> GetAsync(Expression<Func<TAggregate, bool>> filter, CancellationToken ct = default);
 
         TAggregate Add(TAggregate aggregate);
 
--- src/CloudEngineering.CodeOps.Abstractions/Repositories/Repository.cs
+++ src/CloudEngineering.CodeOps.Abstractions/Repositories/Repository.cs
@@ -3,6 +3,7 @@ using CloudEngineering.CodeOps.Abstractions.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CloudEngineering.CodeOps.Abstractions.Repositories
@@ -32,6 +33,6 @@ namespace CloudEngineering.CodeOps.Abstractions.Repositories
 
         public abstract void Delete(TAggregate aggregate);
 
-        public abstract Task<IEnumerable<TAggregate>> GetAsync(Expression<Func<TAggregate, bool>> filter);
+        public abstract Task<IEnumerable<TAggregate>> GetAsync(Expression<Func<TAggregate, bool>> filter, CancellationToken ct = default);
     }
 }
